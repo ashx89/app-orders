3ccfc2a3f764c90ce58eac7fd50ef3f3e735a504
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-14 14:31:35 +0100

    delete card fix

--- components/cards/delete.js
+++ components/cards/delete.js
@@ -1,3 +1,4 @@
+var Account = require(__base + '/app/models/account');
 var cardsApi = require(__payment_base + '/lib/cards');
 
 /**
@@ -5,16 +6,17 @@ var cardsApi = require(__payment_base + '/lib/cards');
  */
 var remove = function onFetch(req, res, next) {
 	var cardId = req.params.id;
-	var customerId = req.user.customer_id;
 
-	cardsApi.fetchAll(customerId, function onFetch(err, cards) {
-		if (err) return next(err);
-		if (!cards.length) return res.status(200).json({});
-		if (cards.length === 1) return next(new Error('There is only one card on your account. Cannot remove'));
-
-		cardsApi.delete(customerId, cardId, function onDelete(err, results) {
+	Account.findOne({ user: req.user._id }, function onFind(err, account) {
+		cardsApi.fetchAll(account.customer_id, function onFetch(err, cards) {
 			if (err) return next(err);
-			return res.status(200).json(results);
+			if (!cards.data.length) return res.status(200).json({});
+			if (cards.data.length === 1) return next(new Error('There is only one card on your account. Cannot remove'));
+
+			cardsApi.delete(account.customer_id, cardId, function onDelete(err, results) {
+				if (err) return next(err);
+				return res.status(200).json(results);
+			});
 		});
 	});
 };
