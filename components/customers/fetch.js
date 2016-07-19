8bdd2ef84f8a8b977d54b96819d3f27f279cc776
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-19 14:32:21 +0100

    added customer fetch api

---
+++ components/customers/fetch.js
@@ -0,0 +1,17 @@
+var Account = require(__base + '/manager').AccountModel;
+var customersApi = require(global.__payment_base + '/lib/customers');
+
+/**
+ * Fetch customer details
+ */
+var fetch = function onFetch(req, res, next) {
+	Account.findOne({ user: req.user._id }, function onFind(err, account) {
+		if (err) return next(err);
+		customersApi.fetch(account.customer_id, function onFetchCustomer(err, customer) {
+			if (err) return next(err);
+			return res.status(200).json(customer);
+		});
+	});
+};
+
+module.exports = fetch;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+global.__payment_base = __dirname;
+
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
-global.__payment_base = __dirname;
 /**
  * Rest:: Card
  */
@@ -10,5 +11,11 @@ app.get('/cards', require('./components/cards/fetch'));
 app.get('/cards/:id', require('./components/cards/fetch'));
 app.post('/cards', require('./components/cards/create'));
 app.delete('/cards/:id', require('./components/cards/delete'));
+
+/**
+ * Rest:: Customer
+ */
+app.get('/customers', require('./components/customers/fetch'));
+app.post('/customers', require('./components/customers/create'));
 
 module.exports = app;
